879d0c34ac6471950a1b950958e9bf147faf82ff
Speecker <jan.speecker.mayer@googlemail.com>
2022-03-29 06:40:06 +0200

    v10.4.0-027

---
+++ Documentation/simple_calculate.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+      
+    int main()
+    {
+        int n0=0, n1=0, n2=0, n3=0, n4=0, n5=0, n6=0, n7=0, n8=0, n9=0, sum=0;
+       
+        n0 = 0 ;
+        n1 = 0 ;
+        n2 = 0 ;
+        n3 = 0 ;
+        n4 = 0 ;
+        n5 = 0 ;
+        n6 = 0 ;
+        n7 = 0 ;
+        n8 = 0 ;
+        n9 = 0 ;
+
+        sum = n0+n1+n2+n3+n4+n5+n6+n7+n8+n9;
+
+        printf("sum = %d\n", sum);
+
+        return 0;
+    }
